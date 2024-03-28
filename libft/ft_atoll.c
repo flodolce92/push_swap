cafbd1e1d1451577416d16ff34c947354f3b2532
Federico Lo Dolce <flodolce92@gmail.com>
2024-03-28 16:45:44 +0100

    update libft

---
+++ libft/ft_atoll.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 16:18:21 by flo-dolc          #+#    #+#             */
+/*   Updated: 2024/03/28 16:18:44 by flo-dolc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long long	ft_atoll(const char *str)
+{
+	int			i;
+	int			sign;
+	long long	res;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
